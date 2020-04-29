615392229e5c37410cf5a4c9caabc73c19568f8e
Alexander Cusianovic <acusianovic@gmail.com>
2020-04-28 21:11:04 -0700

    Fixed fin leading edge bug

--- getRandomRocket.m
+++ getRandomRocket.m
@@ -78,7 +78,6 @@ rocket.geo.fin.sweep = 20 + rand()*(45-20); %degrees, sweep angle
 rocket.geo.fin.S_wet = rocket.geo.fin.S*(1.977 + 0.52*rocket.geo.fin.ThR); %ft^2, wetted area formula from http://www.ipublishing.co.in/jarvol1no12010/EIJAER2011.pdf
 rocket.geo.fin.h_t = 0.3; %nondimensional distance to maximum thickness
 
-rocket.geo.fin.LE = rocket.geo.body.L - rocket.geo.fin.c; % place fin at bottom of the rocket
 
 rocket.geo.fin.h_ac = 0.25; %nondimensional, distance from fin leading edge to AC
 rocket.geo.fin.ac = rocket.geo.fin.h_ac*rocket.geo.fin.c; %ft, distance from fin leading edge to AC, set to quarter chord
